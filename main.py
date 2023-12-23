4584ef0ff44c4989e0a115cb38cde71e633497aa
CSNeko <minecrafthetun@outlook.com>
2023-12-23 12:58:08 +0800

    使其能够正确读取配置文件

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from pathlib import Path
-from file.YamlConfiguration import YamlConfiguration
+import yaml
 
 
 print("喵~QQBot开始启动")
 
 # 读取go-cqhttp的配置文件
-config_path = Path("../config.yml")
-config = YamlConfiguration(config_path)
-address = config.get("servers.0.http.address")  # 读取监听地址
-print(address)
+with open('../config.yml', 'r') as stream:
+    config = yaml.safe_load(stream)
+address = config['servers'][0]['http']['address']  # 读取监听地址
