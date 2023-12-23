4b9253154e33dc6641063acc175060f4d6f83889
CSNeko <minecrafthetun@outlook.com>
2023-12-23 13:27:15 +0800

    更新配置

---
+++ initialize/config.py
@@ -0,0 +1,11 @@
+import yaml
+
+# 读取go-cqhttp的配置文件
+with open('../config.yml', 'r') as stream:
+    go_config = yaml.safe_load(stream)
+address = go_config['servers'][0]['http']['address']  # 监听地址
+
+# 读取配置文件
+with open('config.yml', 'r') as stream:
+    config = yaml.safe_load(stream)
+group = config['group']  # 群号
--- main.py
+++ main.py
@@ -3,7 +3,4 @@ import yaml
 
 print("喵~QQBot开始启动")
 
-# 读取go-cqhttp的配置文件
-with open('../config.yml', 'r') as stream:
-    config = yaml.safe_load(stream)
-address = config['servers'][0]['http']['address']  # 读取监听地址
+
