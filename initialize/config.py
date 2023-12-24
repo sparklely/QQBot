5562b33f1530c622e1020a317d11627b91d549f1
FengXiang2233 <fjx13125@163.com>
2023-12-24 21:12:34 +0800

    fix 编码问题所致无法读取文件

--- initialize/config.py
+++ initialize/config.py
@@ -6,6 +6,6 @@ with open('../config.yml', 'r', encoding='utf-8', errors='ignore') as stream:
 address = "http://" + go_config['servers'][0]['http']['address']  # 监听地址
 address = address.replace("0.0.0.0", "127.0.0.1")  # 把0.0.0.0替换成127.0.0.1
 # 读取配置文件
-with open('config.yml', 'r') as stream:
+with open('config.yml', 'r', encoding='utf-8', errors='ignore') as stream:
     config = yaml.safe_load(stream)
 group = config['group']  # 群号
