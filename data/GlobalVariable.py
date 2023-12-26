7cd543132e582019a939853a9a6baa4f7796d0af
FengXiang2233 <fjx13125@163.com>
2023-12-27 02:01:13 +0800

    添加批注 增加doc数据存储位置配置

--- data/GlobalVariable.py
+++ data/GlobalVariable.py
@@ -1,2 +1,3 @@
+#类变量引用时不会被重置
 class GlobalVariable:
     mysql=None
--- initialize/doc.py
+++ initialize/doc.py
@@ -7,7 +7,7 @@ def Check_file_integrity():
         #检查文件是否存在
         if not os.path.isfile(file_ckeck):
             #初始化
-            with open("./data/"+file_ckeck,"w") as open_doc:
+            with open(config['doc']['path']+file_ckeck,"w") as open_doc:
                 if config['doc']['mode']=='json':
                     open_doc.write("\x7b\x7d")#转义 \x7b->{ \x7d->}
                 elif config['doc']['mode']=='yaml':
