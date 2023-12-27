6683c30215e198c29e60c53e353d4743d53f73e3
CSneko <minecrafthetun@outlook.com>
2023-12-27 23:02:03 +0800

    添加了更多的方法

--- database/data_base.py
+++ database/data_base.py
@@ -1,5 +1,4 @@
 from initialize import vars
-from initialize.config import config
 
 u_m = vars.sql.use_mysql
 my = vars.sql.mysql
@@ -16,4 +15,19 @@ def save_data(table_name, column_name, data):
 
 
 # 检查数据库的某个列中是否有某个值
-def check_value_exists
+# tableName 表名, columnName 列名, value 要检查的值
+def check_value_exists(table_name, column_name, value):
+    if u_m:
+        my.check_value_exists(table_name, column_name, value)
+    else:
+        lite.check_value_exists(table_name, column_name, value)
+
+
+# 在tableName表中，需要更改columnName,当满足whereName = data时，则执行update
+# tableName 表名, columnName 要更改的列(如: balance), whereName 要更改的条件列 (如：username), whereValue 要更改的条件值 (如：CrystalNeko)
+# columnValue 更改后的值(如：123)
+def save_data_where(table_name, column_name, where_name, where_value, column_value):
+    if u_m:
+        my.save_data_where(table_name, column_name, where_name, where_value, column_value)
+    else:
+        lite.save_data_where(table_name, column_name, where_name, where_value, column_value)
