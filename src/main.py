679d1c9fd707e64c0000139ebb708b6dfee71f67
CSneko <minecrafthetun@outlook.com>
2024-01-02 22:36:01 +0800

    添加启动和关闭消息

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import threading
-
+from file import log
 from command import console
 from initialize.event import init_event
 
 # ------------------------------------------------------初始化------------------------------------------------------
-print("喵~开始初始化")
+log.info("喵~开始初始化", True)
 
 # 初始化sql
 from initialize import sql
@@ -25,8 +25,9 @@ while True:
     # 执行命令
     if Type == "stop":
         event.stop()
+        log.log_info("成功关闭程序", True)
         break
     try:
         console.execute(Type)
     except Exception as e:
-        print(f"ERROR: 发生未知错误 {e}，尝试修复")
+        log.log_error(f"发生未知错误 {e}，尝试修复", True)
