bb6fee6aa7a6753b92e8e2ab7b1ee192215c2dae
CSneko <minecrafthetun@outlook.com>
2024-01-03 23:22:24 +0800

    把一些输出替换成了日志

--- src/initialize/sql.py
+++ src/initialize/sql.py
@@ -2,6 +2,7 @@ from initialize.config import config
 from initialize import g_vars
 from database import sqlite_db
 import os
+from file import log
 
 
 # 获取sqlite路径
@@ -10,6 +11,6 @@ if not os.path.exists(path):
     # 文件不存在，创建文件
     with open(path, "w"):
         pass
-    print("成功創建sqlite")
+    log.info("成功創建sqlite文件", True)
     # 连接sqlite
 g_vars.sql.sqlite = sqlite_db.SQLite(path)
