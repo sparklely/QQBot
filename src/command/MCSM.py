e490ef5dd4734f4a812ce4442e216d853c45bda0
FengXiang2233 <fjx13125@163.com>
2024-01-06 13:59:05 +0800

    add MCSM相关模块

---
+++ src/command/MCSM.py
@@ -0,0 +1,82 @@
+from initialize.config import config
+from message import send
+from file import doc,log
+from network import post,get,put
+
+import yaml
+import time
+
+class MCSM:
+    def __init__(self):
+        self.qMCSM=MCSM()
+        self.qq=""
+        self.headers={'X-Requested-With':"XMLHttpRequest",'Content-Type':"application/json; charset=UTF-8"}
+   # 注册
+    def reg(self,msg_data):
+        self.qq=msg_data["sender"]["user_id"]
+        data={
+            "username":msg_data["sender"]["user_id"],
+            "password":str(msg_data["sender"]["user_id"])+"@Ab123456",
+            "permission":"1"
+        }
+        try:
+            re=post.json_headers(config["MCSM"]["url"]+"/api/auth?apikey="+config["MCSM"]["apikey"],self.headers,data)
+            if re.json()['data']==True:
+                if config["MCSM"]["AAI"]["enable"]:
+                    self.qMCSM.AAI()
+                send.group_msg(f"[CQ:at,qq={self.qq}]注册成功\n-------------------------------------\n默认密码:{self.qq}@Ab123456\n请及时进入面板更改密码\n若忘记密码请让管理员帮忙重置",False)
+            elif re.json()['data']=='用户名已经被占用':
+                send.group_msg(f"[CQ:at,qq={self.qq}]禁止重复注册",False)
+            else:
+                print(re.json())
+                send.group_msg(f"[CQ:at,qq={self.qq}]未知错误,请联系管理员查看后台报错",False)
+        except:
+            log.error("无法请求MCSM注册api",True)
+            send.group_msg(f"[CQ:at,qq={self.qq}]无法请求到api",False)
+    # 创建实例
+    def CI(self):
+        MCSM_port=doc.yaml_read("./res/MCSM/port.yaml")
+        port=str(MCSM_port['port'])
+        ticks = time.time()
+        # instance数据处理
+        data=doc.json_read("./res/MCSM/init_instance.json")
+        data['nickname']=config["MCSM"]["url"]+":"+port
+        data['createDatetime']=ticks
+        data['lastDatetime']=ticks
+        data_docker_ports=[]
+        for port_type in config["MCSM"]["AAI"]["port"]["type"]:
+            data_docker_ports.append(f"{port}:{port}/{port_type}")
+        data['docker']['ports']=data_docker_ports
+        data['startCommand']=config["MCSM"]["AAI"]["startCommand"]
+        data['stopCommand']=config["MCSM"]["AAI"]["stopCommand"]
+        data['type']=config["MCSM"]["AAI"]["type"]
+        data['endTime']=config["MCSM"]["AAI"]["endTime"]
+        data['docker']['image']=config["MCSM"]["AAI"]["docker"]["image"]
+        data['docker']['memory']=config["MCSM"]["AAI"]["docker"]["memory"]
+        data['docker']['cpusetCpus']=config["MCSM"]["AAI"]["docker"]["cpusetCpus"]
+        return post.json_headers(config["MCSM"]["url"]+"/api/instance?apikey="+config["MCSM"]["apikey"]+"&remote_uuid="+config["MCSM"]["AAI"]["remote_uuid"],self.headers,data).json()
+    # 自动分配实例
+    def AAI(self):
+        CIData=self.qMCSM.CI()
+        # 查询用户uuid
+        uuid=get.get_json(config["MCSM"]["url"]+"/api/auth/search?apikey="+config["MCSM"]["apikey"]+"&userName="+self.qq+"&page=1&page_size=10000")['data']['data'][0]['uuid']
+        # 分配实例数据处理
+        data={
+            "uuid":uuid,
+            "apikey":config["MCSM"]["apikey"],
+            "config":{
+                "permission": 1,
+                "instances": [
+                    {
+                        "instanceUuid":CIData["data"]["instanceUuid"],
+                        "serviceUuid":config["MCSM"]["AAI"]["remote_uuid"]
+                    }
+                ]
+            }
+        }
+        put.headers_json(config["MCSM"]["url"]+"/api/auth/?apikey="+config["MCSM"]["apikey"],self.headers,data)
+        MCSM_port=doc.yaml_read("./res/MCSM/port.yaml")
+        port=str(MCSM_port['port'])
+        open_doc=open("./res/MCSM/port.yaml","w")
+        open_doc.write(yaml.dump({"port":int(port)+1}))
+        open_doc.close()
--- src/command/user.py
+++ src/command/user.py
@@ -1,33 +1,33 @@
-from command import help, yiyan, ai, acg
+from command import help, yiyan, ai, acg,MCSM
 from initialize import config
 from initialize.config import command_async
 import asyncio
 import threading
 
 
-def execute(c_type):
+def execute(c_type,msg_data):
     if command_async:
-        thread = threading.Thread(target=start_async, args=(c_type,))
+        thread = threading.Thread(target=start_async, args=(c_type,msg_data,))
         thread.start()
 
     else:
         _execute(c_type)
 
 
-def start_async(c_type):
+def start_async(c_type,msg_data):
     # 在新线程中创建事件循环并运行异步任务
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.run_until_complete(async_execute(c_type))
+    loop.run_until_complete(async_execute(c_type,msg_data))
     loop.close()
 
 
-async def async_execute(c_type):
+async def async_execute(c_type,msg_data):
     # 通过异步线程执行
-    _execute(c_type)
+    _execute(c_type,msg_data)
 
 
-def _execute(c_type):
+def _execute(c_type,msg_data):
     # 转换为数组
     if c_type == "":
         return
@@ -44,3 +44,6 @@ def _execute(c_type):
         yiyan.yy()
     if arr_type[0] == "二次元" and config.random_agc_enable:
         acg.random_img()
+    if arr_type[0]=="注册":
+        pMCSM=MCSM()
+        pMCSM.reg(msg_data)
