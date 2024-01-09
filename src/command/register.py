a9612213c685745146d60b474c4bf7fb7ee8e339
CSneko <minecrafthetun@outlook.com>
2024-01-09 22:23:59 +0800

    修改了一点命令注册逻辑

--- src/command/register.py
+++ src/command/register.py
@@ -1,11 +1,9 @@
-def reg(plug, cmd, des, module):
+def reg(cmd, cla):
     """
    注册命令
 
    Args:
-    plug (str) : 插件名称
     cmd (str) : 命令主体
-    des (str) : 命令描述
-    module (ModuleType) : 模块
+    cla (class) : 类
 
    """
