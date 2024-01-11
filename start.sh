28a342c0d1acdb29d1eeb0ba288766fddd9c54f2
CSneko <minecrafthetun@outlook.com>
2024-01-11 22:42:14 +0800

    更新start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 path="src/main.py"
+# 尝试执行 py 命令
+if hash py 2>/dev/null; then
+    py ${path}
+# 如果不存在 py 命令，尝试执行 python 命令
+elif hash python 2>/dev/null; then
+    python ${path}
+# 如果不存在 python 命令，尝试执行 python3 命令
+elif hash python3 2>/dev/null; then
+    python3 ${path}
+# 如果都不存在，输出提示信息并退出脚本
+else
+    echo "找不到 Python 解释器"
+    exit 1
+fi
 
-# 运行python
-python3 ${path}
